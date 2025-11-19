5d4971bb732e0ca372dee10ab080ae4d7c4626ba
Muhammad Mustafa Giani <bscs2312161@szabist.pk>
2025-11-19 18:18:31 +0500

    Initial Project Upload

---
+++ Week_8_SCDLab/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Week_8_SCDLab.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
